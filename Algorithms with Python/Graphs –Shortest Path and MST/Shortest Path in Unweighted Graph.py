9352b6db537ee7f6baf40d46204f823f477e2a3c
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2022-07-25 17:17:22 +0300

    Algorithms

---
+++ Algorithms with Python/Graphs –Shortest Path and MST/Shortest Path in Unweighted Graph.py
@@ -0,0 +1,37 @@
+
+from collections import deque
+
+
+nodes = int(input())
+edges = int(input())
+graph = []
+[graph.append([]) for _ in range(nodes + 1)]
+for _ in range(edges):
+    source, dest = [int(x) for x in input().split(' ')]
+    graph[source].append(dest)
+
+start_node = int(input())
+dest_node = int(input())
+visited = [False] * (nodes + 1)
+parent = [None] * (nodes + 1)
+visited[start_node] = True
+queue = deque([start_node])
+while queue:
+    node = queue.popleft()
+    if node == dest_node:
+        break
+    for child in graph[node]:
+        if visited[child]:
+            continue
+        visited[child] = True
+        queue.append(child)
+        parent[child] = node
+
+path = deque()
+node = dest_node
+while node is not None:
+    path.appendleft(node)
+    node = parent[node]
+
+print(f'Shortest path length is: {len(path) - 1}')
+print(*path, sep=' ')
