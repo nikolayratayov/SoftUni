2a03f9402e06aadcbd6015b082b863adc0891e84
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2022-08-04 09:07:36 +0300

    Final exam

--- Python Fundamentals/Final exams/1/Ad astra.py
+++ Python Fundamentals/Final exams/1/Ad astra.py
@@ -1,12 +1,13 @@
 import re
 
 a = input()
-regex = r'[#||]([A-Za-z\s]+)[#||](\d{2}/\d{2}/\d{2})[#||](\d+)[#||]'
+regex = r'([#\|])([A-Za-z\s]+)\1(\d{2}/\d{2}/\d{2})\1(\d+)\1'
 food = 0
 x = re.findall(regex, a)
 for z in x:
-    food += int(z[2])
+    food += int(z[3])
 food = food // 2000
 print(f'You have food to last you for: {food} days!')
 for maikati in x:
-    print(f'Item: {maikati[0]}, Best before: {maikati[1]}, Nutrition: {maikati[2]}')
+    if 0 <= int(maikati[3]) <= 10000:
+        print(f'Item: {maikati[1]}, Best before: {maikati[2]}, Nutrition: {maikati[3]}')
