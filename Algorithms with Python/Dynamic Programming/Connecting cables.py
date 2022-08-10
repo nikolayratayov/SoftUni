f75d0fb1bc5743c581eccb52c9d96a021062a1e8
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2022-08-10 08:51:37 +0300

    Algorithms

---
+++ Algorithms with Python/Dynamic Programming/Connecting cables.py
@@ -0,0 +1,14 @@
+cables = [int(x) for x in input().split()]
+positions = [x for x in range(1, len(cables) + 1)]
+size = len(cables) + 1
+lcs = []
+[lcs.append([0] * size) for _ in range(size)]
+
+for row in range(1, size):
+    for col in range(1, size):
+        if cables[row - 1] == positions[col - 1]:
+            lcs[row][col] = lcs[row - 1][col - 1] + 1
+        else:
+            lcs[row][col] = max(lcs[row - 1][col], lcs[row][col - 1])
+
+print(f'Maximum pairs connected: {lcs[size - 1][size - 1]}')
