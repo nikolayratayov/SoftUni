4b48c616d047be110f60eecb5bbc39b4415d5926
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2022-08-28 09:28:43 +0300

    js

---
+++ JS advanced/JS advanced/DOM introduction/Lab/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,5 @@
+function calc() {
+    a = parseFloat(document.getElementById('num1').value);
+    b = parseFloat(document.getElementById('num2').value);
+    document.getElementById('sum').value = a + b;
+}
