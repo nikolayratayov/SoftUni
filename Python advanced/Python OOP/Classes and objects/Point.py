eb7c6c7b9d0c89188cbcb817ef1919217866fe87
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2022-10-26 16:49:43 +0300

    python OOP

---
+++ Python advanced/Python OOP/Classes and objects/Point.py
@@ -0,0 +1,14 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def set_x(self, x):
+        self.x = x
+
+    def set_y(self, y):
+        self.y = y
+
+    def __str__(self):
+        return f'The point has coordinates ({self.x},{self.y})'
+    
