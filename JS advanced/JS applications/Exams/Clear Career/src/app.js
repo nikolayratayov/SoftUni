3b89c9e3323db666278b5fe4fbd30ec1fe800d7e
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2022-11-28 16:47:01 +0200

    js apps

---
+++ JS advanced/JS applications/Exams/Clear Career/src/app.js
@@ -0,0 +1,15 @@
+import {page, render} from './api/lib.js';
+import * as api from './api/user.js'
+
+
+page('/', () => console.log('home'));
+page('/catalog', () => console.log('catalog'));
+page('/catalog/:id', () => console.log('details'));
+page('/edit/:id', () => console.log('edit'));
+page('/create', () => console.log('create'));
+page('/login', () => console.log('login'));
+page('/register', () => console.log('register'));
+
+page.start();
+
+window.api = api;
---
+++ JS advanced/JS applications/Exams/Clear Career/src/util.js
@@ -0,0 +1,23 @@
+export function getUserData(){
+    let data = JSON.parse(sessionStorage.getItem('userData'));
+    return data;
+}
+
+export function setUserData(data){
+    sessionStorage.setItem('userData', JSON.stringify(data));
+}
+
+export function clearUserData(){
+    sessionStorage.removeItem('userData');
+}
+
+export function createSubmitHandler(callabck){
+    return function (event){
+        event.preventDefault();
+        let formData = new FormData(event.target);
+        let data = Object.fromEntries(formData);
+
+        callabck(data);
+
+    }
+}
